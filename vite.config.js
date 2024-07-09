5cf03449f120834ae5492e796fb3227e905bfccf
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-07-09 10:30:11 +0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// Vite configaration file:
+
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
